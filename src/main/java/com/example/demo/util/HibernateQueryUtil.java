5f23db0972990fcea2d2a11584d5c6c6bee7af90
bala-amypo <bala@amypo.in>
2025-12-26 10:10:04 +0000

    User commit

--- src/main/java/com/example/demo/util/HibernateQueryUtil.java
+++ src/main/java/com/example/demo/util/HibernateQueryUtil.java
@@ -1,8 +1,26 @@
-package com.example.demo.security;
+package com.example.demo.util;
 
-public class SecurityConstants {
-    public static final String SECRET = "SecretKeyForJwt";
-    public static final long EXPIRATION_TIME = 86400000;
-    public static final String TOKEN_PREFIX = "Bearer ";
-    public static final String HEADER_STRING = "Authorization";
+import com.example.demo.model.ApprovalAction;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.*;
+import javax.persistence.criteria.*;
+import java.util.List;
+
+@Component
+public class HibernateQueryUtil {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public List<ApprovalAction> findActionsByApproverUsingCriteria(Long approverId) {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<ApprovalAction> cq = cb.createQuery(ApprovalAction.class);
+        Root<ApprovalAction> root = cq.from(ApprovalAction.class);
+
+        cq.select(root)
+          .where(cb.equal(root.get("approverId"), approverId));
+
+        return em.createQuery(cq).getResultList();
+    }
 }
