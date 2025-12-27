b4d34d1ac17b23f5d495ae20666f59384c660a60
bala-amypo <bala@amypo.in>
2025-12-27 04:55:30 +0000

    User commit

--- src/main/java/com/example/demo/repository/WorkflowStepConfigRepository.java
+++ src/main/java/com/example/demo/repository/WorkflowStepConfigRepository.java
@@ -2,11 +2,12 @@ package com.example.demo.repository;
 
 import com.example.demo.model.WorkflowStepConfig;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import java.util.List;
 
-public interface WorkflowStepConfigRepository
-        extends JpaRepository<WorkflowStepConfig, Long> {
-
+@Repository
+public interface WorkflowStepConfigRepository extends JpaRepository<WorkflowStepConfig, Long> {
     List<WorkflowStepConfig> findByTemplateIdOrderByLevelNumberAsc(Long templateId);
 }
--- src/main/java/com/example/demo/security/SecurityConstants.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.security;
-
-public class SecurityConstants {
-    public static final String SECRET = "SecretKey";
-}
